572c3921eb64aeef08c1815ce39e2b746810f3e3
Igor Achkas <jigoren@yandex.ru>
2023-07-03 23:07:06 +0700

    add 1

---
+++ my_first_cmake_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name;
+	std::cout << "Введите имя: " ;
+	std::cin >> name;
+	std::cout << "Здравствуйте, "<< name ;
+	return 0;
+}
